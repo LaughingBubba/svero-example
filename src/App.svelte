9744885a2374b33cc2073005bb73b0fbdd61e819
laughingbubba <laughingbubba@outlook.com>
2019-08-13 22:21:22 +1000

    forgot to add exact to root path

--- src/App.svelte
+++ src/App.svelte
@@ -12,7 +12,7 @@
 <button on:click={()=>navigateTo('/stooges')}>Show Stooges</button>
 
 <Router>
-	<Route path='/'>
+	<Route exact path='/'>
 		<h1>Home</h1>
 	</Route>
 	<!-- <Route exact path='/stooges/add' component={AddStooge}/> -->
